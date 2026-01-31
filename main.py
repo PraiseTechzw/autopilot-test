024b42629d4daf11bd64de8ed9a21ac3e180eca8
Praise Masunga <praisesamasunga04@gmail.com>
2026-02-01 01:09:57 +0200

    fix: update time analytics and correct main.py print statements

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 print(hello) = "Hello, World!"
 print(hello)
 
+gareeting = "Welcome to Python programming."
+print(gareeting)
+
+farewell = "Goodbye, see you later!"
+print(farewell)
+
+
+print(hello) = "Hello, World!"
+print(hello)
+
+hasattr(gareeting, 'upper')
+print(gareeting.upper())    
